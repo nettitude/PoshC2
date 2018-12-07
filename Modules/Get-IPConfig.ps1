10a2e9dda5636dac8c5e9d8a6be696e4bbe48b51
benpturner <bturner@nettitude.com>
2018-12-07 14:25:22 +0000

    Updated Get-IPAddress

--- Modules/Get-IPConfig.ps1
+++ Modules/Get-IPConfig.ps1
@@ -2,35 +2,30 @@
 
 function Get-Ipconfig {
 
-$array= @()  
+$array= @()
   
+$wmi=Get-WmiObject win32_ComputerSystem
   
-$wmi=Get-WmiObject win32_ComputerSystem  
+$obj=New-Object PSObject
   
-$obj=New-Object PSObject  
-  
-$obj |Add-Member -MemberType NoteProperty -Name "ComputerName" $wmi.Name  
+$obj |Add-Member -MemberType NoteProperty -Name "ComputerName" $wmi.Name
  
-$obj |Add-Member -MemberType NoteProperty -Name "IPAddress" $IP  
- 
+# Finding the Network Adapter and MAC Address, DHCP Server
   
-# Finding the Network Adapter and MAC Address, DHCP Server  
-  
-$wmi=Get-WmiObject win32_networkadapterconfiguration | where {$_.Ipenabled -Match "True"}  
-  
-$obj |Add-Member -MemberType NoteProperty -Name "NetworkAdapter" $wmi.description  
-$obj |Add-Member -MemberType NoteProperty -Name "MACAddress" $wmi.macaddress  
-$obj |Add-Member -MemberType NoteProperty -Name "DefaultGateway" $wmi.DefaultIPGateway 
-$obj |Add-Member -MemberType NoteProperty -Name "DHCPServer" $wmi.DHCPServer 
-$obj |Add-Member -MemberType NoteProperty -Name "DHCPEnabled" $wmi.DHCPEnabled 
-$obj |Add-Member -MemberType NoteProperty -Name "SubnetMask" $wmi.IPSubnet 
-$obj |Add-Member -MemberType NoteProperty -Name "DNSServer" $wmi.DnsServerSearchOrder 
-$obj |Add-Member -MemberType NoteProperty -Name "WinsPrimaryServer" $wmi.WinsPrimaryServer 
-$obj |Add-Member -MemberType NoteProperty -Name "WinsSecondaryServer" $wmi.WinsSecondaryServer 
-$array +=$obj  
+$wmi=Get-WmiObject win32_networkadapterconfiguration | where {$_.Ipenabled -Match "True"}
+$obj |Add-Member -MemberType NoteProperty -Name "IPAddress" $wmi.IPAddress
+$obj |Add-Member -MemberType NoteProperty -Name "NetworkAdapter" $wmi.description
+$obj |Add-Member -MemberType NoteProperty -Name "MACAddress" $wmi.macaddress
+$obj |Add-Member -MemberType NoteProperty -Name "DefaultGateway" $wmi.DefaultIPGateway
+$obj |Add-Member -MemberType NoteProperty -Name "DHCPServer" $wmi.DHCPServer
+$obj |Add-Member -MemberType NoteProperty -Name "DHCPEnabled" $wmi.DHCPEnabled
+$obj |Add-Member -MemberType NoteProperty -Name "SubnetMask" $wmi.IPSubnet
+$obj |Add-Member -MemberType NoteProperty -Name "DNSServer" $wmi.DnsServerSearchOrder
+$obj |Add-Member -MemberType NoteProperty -Name "WinsPrimaryServer" $wmi.WinsPrimaryServer
+$obj |Add-Member -MemberType NoteProperty -Name "WinsSecondaryServer" $wmi.WinsSecondaryServer
+$array +=$obj
 
 echo "[+] IPConfig "
 echo $array
 
 }
-
