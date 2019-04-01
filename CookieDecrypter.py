ad1e6e26184bcc7219ac715bbf435bfdd26aa458
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2019-04-01 20:46:40 +0100

    Fix indenting

--- CookieDecrypter.py
+++ CookieDecrypter.py
@@ -8,13 +8,13 @@ import os, sys, re
 file = open(sys.argv[1], "r")
 result = get_keys()
 
-for line in file:
-     if re.search("SessionID", line):
-	 if result:
-  	   for i in result:
-    	    try:
-             value = decrypt(i[0], line.split('=')[1]) 
-	     print (Colours.GREEN + "Success with Key %s - %s" % (i[0],value))
-            except: 
-             print (Colours.RED + "Failed with Key %s" % i[0])
+if result:
+     for line in file:
+          if re.search("SessionID", line):
+          	for i in result:
+                    try:
+                         value = decrypt(i[0], line.split('=')[1]) 
+                         print (Colours.GREEN + "Success with Key %s - %s" % (i[0],value))
+                    except: 
+                         print (Colours.RED + "Failed with Key %s" % i[0])
 
