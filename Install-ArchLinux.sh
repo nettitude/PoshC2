b549f0378c1c32c2a8689db1d57094d7c2e1c3b6
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2019-06-19 19:01:54 +0100

    Update Arch install script

--- Install-ArchLinux.sh
+++ Install-ArchLinux.sh
@@ -16,7 +16,7 @@ echo ""
 
 # Update apt
 echo "[+] Performing apt-get update"
-apt-get update
+sudo pacman -Syu
 
 # Check if /opt/ exists, else create folder opt
 if [ ! -d /opt/ ]; then
@@ -28,14 +28,16 @@ fi
 # Git cloning PoshC2_Python
 echo ""
 echo "[+] Installing git & cloning PoshC2_Python into /opt/PoshC2_Python/"
-apt-get install -y git
-git clone https://github.com/nettitude/PoshC2_Python /opt/PoshC2_Python/
+sudo pacman -Sy git
+sudo git clone https://github.com/nettitude/PoshC2_Python /opt/PoshC2_Python/
 
 # Install requirements for PoshC2_Python
 echo ""
-echo "[+] Installing requirements using apt"
-apt-get install -y screen python-setuptools python3 python3-dev python3-pip python-dev build-essential python-pip mingw-w64-tools mingw-w64 mingw-w64-x86-64-dev mingw-w64-i686-dev mingw-w64-common espeak graphviz mono-complete apt-transport-https
-  
+echo "[+] Installing requirements using pacman"
+sudo pacman -Sy screen python3 python-pip espeak graphviz mono
+pamac build mingw-w64-gcc-base
+pamac build mingw-w64-gcc mono-git
+
 # Setting the minimum protocol to TLS1.0 to allow the python server to support TLSv1.0+
 echo ""
 echo "[+] Updating TLS protocol minimum version in /etc/ssl/openssl.cnf"
@@ -57,44 +59,33 @@ echo ""
 python3 -m pip install --upgrade pip > /dev/null
 python3 -m pip install pipenv > /dev/null
 cd /opt/PoshC2_Python
-rm Pipfile >/dev/null 2>/dev/null
+sudo rm Pipfile >/dev/null 2>/dev/null
 python3 -m pipenv --python 3 run pip install -r /opt/PoshC2_Python/requirements.txt >/dev/null
 
 echo ""
 echo "[+] Copying useful scripts to /usr/bin"
-cp /opt/PoshC2_Python/Files/fpc /usr/bin
-cp /opt/PoshC2_Python/Files/fpc.py /usr/bin
-cp /opt/PoshC2_Python/Files/posh /usr/bin
-cp /opt/PoshC2_Python/Files/posh-server /usr/bin
-cp /opt/PoshC2_Python/Files/posh-config /usr/bin
-cp /opt/PoshC2_Python/Files/posh-log /usr/bin
-cp /opt/PoshC2_Python/Files/posh-service /usr/bin
-chmod +x /usr/bin/fpc
-chmod +x /usr/bin/fpc.py
-chmod +x /usr/bin/posh
-chmod +x /usr/bin/posh-server
-chmod +x /usr/bin/posh-config
-chmod +x /usr/bin/posh-log
-chmod +x /usr/bin/posh-service
+sudo cp /opt/PoshC2_Python/Files/fpc /usr/bin
+sudo cp /opt/PoshC2_Python/Files/fpc.py /usr/bin
+sudo cp /opt/PoshC2_Python/Files/posh /usr/bin
+sudo cp /opt/PoshC2_Python/Files/posh-server /usr/bin
+sudo cp /opt/PoshC2_Python/Files/posh-config /usr/bin
+sudo cp /opt/PoshC2_Python/Files/posh-log /usr/bin
+sudo cp /opt/PoshC2_Python/Files/posh-service /usr/bin
+sudo chmod +x /usr/bin/fpc
+sudo chmod +x /usr/bin/fpc.py
+sudo chmod +x /usr/bin/posh
+sudo chmod +x /usr/bin/posh-server
+sudo chmod +x /usr/bin/posh-config
+sudo chmod +x /usr/bin/posh-log
+sudo chmod +x /usr/bin/posh-service
 
 echo "[+] Adding service file"
-cp /opt/PoshC2_Python/poshc2.service /lib/systemd/system/poshc2.service
+sudo cp /opt/PoshC2_Python/poshc2.service /lib/systemd/system/poshc2.service
 
 # Install requirements of dotnet core for SharpSocks
 echo ""
-echo "[+] Adding microsoft debian repository & subsequent"
-apt-key adv --keyserver packages.microsoft.com --recv-keys EB3E94ADBE1229CF
-apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/dotnetdev.list
-apt-get update
-
-wget http://ftp.us.debian.org/debian/pool/main/i/icu/libicu57_57.1-6+deb9u2_amd64.deb -O /tmp/libicu57_57.1-6+deb9u2_amd64.deb
-dpkg -i /tmp/libicu57_57.1-6+deb9u2_amd64.deb
-
-wget http://ftp.us.debian.org/debian/pool/main/o/openssl1.0/libssl1.0.2_1.0.2r-1~deb9u1_amd64.deb -O /tmp/libssl1.0.2_1.0.2r-1~deb9u1_amd64.deb
-dpkg -i /tmp/libssl1.0.2_1.0.2r-1\~deb9u1_amd64.deb
-
-apt-get install dotnet-runtime-2.2 dotnet-hostfxr-2.2 dotnet-host
+echo "[+] Adding dotnet core for Sharpsocks"
+sudo pacman -S dotnet-runtime dotnet-host lib32-openssl-1.0 openssl-1.0
 
 echo ""
 echo "[+] Setup complete"
