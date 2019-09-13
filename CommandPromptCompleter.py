c08e43238bc1219efdac73aecdecce6d18d32826
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2019-09-13 15:32:35 +0100

    Prompt improvements

--- CommandPromptCompleter.py
+++ CommandPromptCompleter.py
@@ -4,6 +4,7 @@ from prompt_toolkit.document import Document
 from prompt_toolkit.completion import FuzzyWordCompleter, Completer, CompleteEvent, Completion
 from typing import Callable, Dict, Iterable, List, Optional, Union
 
+
 class FirstWordFuzzyWordCompleter(Completer):
 
     def __init__(self, words: Union[List[str], Callable[[], List[str]]],
@@ -17,7 +18,7 @@ class FirstWordFuzzyWordCompleter(Completer):
         self.fuzzy_word_completer = FuzzyWordCompleter(words=self.words, WORD=self.WORD)
 
     def get_completions(self, document: Document, complete_event: CompleteEvent) -> Iterable[Completion]:
-        pattern = re.compile(r"^\w*$")
+        pattern = re.compile(r"^[^\s]*$")
         if not pattern.match(document.text.strip()):
             return []
         return self.fuzzy_word_completer.get_completions(document, complete_event)
@@ -35,7 +36,7 @@ class FilePathCompleter(Completer):
         self.glob = glob
 
     def get_completions(self, document: Document, complete_event: CompleteEvent) -> Iterable[Completion]:
-        pattern = re.compile(r"^\w*$")
+        pattern = re.compile(r"^[^\s]*$")
         if not pattern.match(document.text.strip()):
             return []
         words = [x.replace(self.path, "") for x in glob.glob(self.path + document.text.strip() + self.glob)]
