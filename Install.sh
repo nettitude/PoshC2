faaab6b766594d4d7aec1b8726569b4cc69c1d14
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-01-20 11:54:54 +0000

    Add python3.8-dev to apt install to fix install issue

--- Install.sh
+++ Install.sh
@@ -47,7 +47,7 @@ fi
 # Install requirements for PoshC2
 echo ""
 echo "[+] Installing requirements using apt"
-apt-get install -y screen python3 python3-dev python3-pip build-essential mingw-w64-tools mingw-w64 mingw-w64-x86-64-dev mingw-w64-i686-dev mingw-w64-common espeak graphviz mono-complete apt-transport-https vim nano python2.7
+apt-get install -y screen python3 python3-dev python3.8-dev python3-pip build-essential mingw-w64-tools mingw-w64 mingw-w64-x86-64-dev mingw-w64-i686-dev mingw-w64-common espeak graphviz mono-complete apt-transport-https vim nano python2.7
 
 # Setting the minimum protocol to TLS1.0 to allow the python server to support TLSv1.0+
 echo ""
