6b0dd8f41243e48616b187f5ed9e6e8bb0405b44
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-04-09 10:12:11 +0100

    SharpHandler cleanup and add new Aliases

--- poshc2/client/Alias.py
+++ poshc2/client/Alias.py
@@ -12,6 +12,7 @@ py_alias = [
 # C# Implant
 cs_alias = [
     ["s", "get-screenshot"],
+    ["ps", "get-processlist"]
 ]
 
 # Parts of commands to replace if command starts with the key
@@ -28,9 +29,10 @@ cs_replace = [
     ["wmiexec", "run-exe WMIExec.Program WExec"],
     ["smbexec", "run-exe SMBExec.Program SExec"],
     ["dcomexec", "run-exe Invoke_DCOM.Program DCOM"],
-    ["sharpsc","run-exe SharpSC.Program SharpSC"],
+    ["sharpsc", "run-exe SharpSC.Program SharpSC"],
     ["sharpcookiemonster", "run-exe SharpCookieMonster.Program SharpCookieMonster"],
-    ["runas","run-exe MainClass RunasCs"],
-    ["runasps","run-exe RunAs.Program RunAs"],
-    ["sharpps","run-exe Program PS"]
+    ["mimikatz", "run-dll SharpSploit.Credentials.Mimikatz SharpSploit"],
+    ["runas", "run-exe MainClass RunasCs"],
+    ["runasps", "run-exe RunAs.Program RunAs"],
+    ["sharpps", "run-exe Program PS"]
 ]
