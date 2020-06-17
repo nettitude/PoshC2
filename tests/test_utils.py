c6ca3fd00f6d1a1dabd960baced926114a216469
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-06-17 15:32:03 +0100

    Check killdate value is a date

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from poshc2.Utils import validate_sleep_time
+from poshc2.Utils import validate_sleep_time, validate_killdate
 
 
 def test_validate_sleep_time():
@@ -15,3 +15,14 @@ def test_validate_sleep_time():
     assert validate_sleep_time("asdf") is None
     assert validate_sleep_time("") is None
     assert validate_sleep_time(None) is None
+
+
+def test_validate_killdate():
+    assert not validate_killdate("0123-45-67")
+    assert not validate_killdate("0000-00-0")
+    assert not validate_killdate("bad")
+    assert not validate_killdate("")
+    assert not validate_killdate("2020-01-45")
+    assert not validate_killdate("2020-13-01")
+    assert validate_killdate("2020-01-01")
+    assert validate_killdate(" 2020-01-01 ")
