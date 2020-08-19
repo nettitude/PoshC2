a690ac7c48a651b94d00e839d0a2982e29ebd9fb
h3x-agram <45673440+h3x-agram@users.noreply.github.com>
2020-08-19 22:07:40 +0100

    Update Get-CreditCardData.ps1
    commenting out line 16 as it's not used and can cause an error

--- resources/modules/Get-CreditCardData.ps1
+++ resources/modules/Get-CreditCardData.ps1
@@ -13,7 +13,7 @@ Function Get-CreditCardData {
       [string]$path = $(throw "-path is required";)
     )
 
-    $Excel = New-Object -ComObject Excel.Application
+    #$Excel = New-Object -ComObject Excel.Application
 
     $REGEX = [regex]"(?im)(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})"
     $REGEX2 = [regex]"^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$"
