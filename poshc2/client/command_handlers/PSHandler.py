7fbde831784f13db137b56b59424e0b0ed2f12a3
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-10-12 11:47:29 +0100

    Quote args in PS startdaisy

--- poshc2/client/command_handlers/PSHandler.py
+++ poshc2/client/command_handlers/PSHandler.py
@@ -729,11 +729,11 @@ def do_startdaisy(user, command, randomuri):
     if domain_front:
         command = command + f" -domfront {domain_front}"
     if proxy_url:
-        command = command + f" -proxyurl {proxy_url}"
+        command = command + f" -proxyurl '{proxy_url}'"
     if proxy_user:
-        command = command + f" -proxyuser {proxy_user}"
+        command = command + f" -proxyuser '{proxy_user}'"
     if proxy_pass:
-        command = command + f" -proxypassword {proxy_pass}"
+        command = command + f" -proxypassword '{proxy_pass}'"
 
     if elevated.lower() == "y" or elevated == "":
 
