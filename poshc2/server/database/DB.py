d91b4196fe86eed1d1b66875b78930b29f33c8d3
John Moss <john@stolen-bikes.co.uk>
2020-12-07 15:13:12 +0000

    select * needed number of columns returned updating

--- poshc2/server/database/DB.py
+++ poshc2/server/database/DB.py
@@ -96,7 +96,7 @@ def get_c2server_all():
     result = c.fetchone()
     return C2(result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9],
               result[10], result[11], result[12], result[13], result[14], result[15], result[16], result[17],
-              result[18], result[19], result[20])
+              result[18], result[19], result[20], result[21], result[22], result[23])
 
 
 def get_implants_all():
